d3f7c3976c204d857dc150bec094c435db3108e8
Jesus Memije <jezu.gome17@gmail.com>
2019-09-22 13:23:17 -0500

    Routes

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,14 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+| Route::get('/', function () {
+|   return view('welcome');
+| });
+|
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+#Route::get('permiso', 'PermisoController@index');
+
+#pasar parámetros
+Route::get('permiso/{nombre}/{slug?}', 'PermisoController@index');
